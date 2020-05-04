d6ec21a55349ea644a61f008e697e27909e63a14
Rafael Carvalho <rafaelherik@gmail.com>
2020-05-04 15:33:29 +0100

    modify configurations and remove default controller WeatherForecast

---
+++ src/FastAPI.Domain.Service/UserDomainServicce.cs
@@ -0,0 +1,18 @@
+﻿using FastAPI.Domain.Abstractions.Repositories;
+using FastAPI.Domain.Abstractions.Services;
+using FastAPI.Domain.Entities;
+
+namespace FastAPI.Domain.Services
+{
+    public class UserDomainService : DomainService<User> , IUserDomainService
+    {
+        public UserDomainService(IRepository<User> repository) : base(repository)
+        {
+        }
+
+        public override bool IsValid(User entity)
+        {
+            return true;
+        }
+    }
+}
